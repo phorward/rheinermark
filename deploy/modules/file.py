41f1b2d09068f3e9ef1b2be109221bda8054471a
Jan Max Meyer <jmm@phorward.de>
2019-11-18 22:59:20 +0100

    Cleaning up rewrite snippets

--- deploy/modules/file.py
+++ deploy/modules/file.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from server.modules.file import File
-from server import utils, db, exposed
+from server import utils
 
 from skeletons.file import fileSkel
 
@@ -17,48 +17,3 @@ class file(File):
 			return res
 
 		return []
-
-	@exposed
-	def rewriteFiles(self, cursor=None):
-		q = self.viewLeafSkel().all()
-		q.cursor(cursor)
-
-		repo = self.getAvailableRootNodes()[0]["key"]
-
-		fetched = False
-		for skel in q.fetch(limit=99):
-			if skel["parentrepo"] != repo:
-				if skel["parentdir"] == skel["parentrepo"]:
-					skel["parentdir"] = repo
-
-				skel["parentrepo"] = repo
-				skel.toDB(clearUpdateTag=True)
-
-			fetched = True
-
-		if fetched:
-			self.rewriteFiles(q.getCursor().urlsafe())
-
-	@exposed
-	def rewriteDirs(self, cursor=None):
-		q = self.viewNodeSkel().all()
-		q.cursor(cursor)
-
-		repo = self.getAvailableRootNodes()[0]["key"]
-
-		fetched = False
-		for entity in q.run(limit=99):
-			if "rootNode" in entity and entity["rootNode"]:
-				continue
-
-			if entity["parentrepo"] != repo:
-				if entity["parentdir"] == entity["parentrepo"]:
-					entity["parentdir"] = repo
-
-				entity["parentrepo"] = repo
-				db.Put(entity)
-
-			fetched = True
-
-		if fetched:
-			self.rewriteDirs(q.getCursor().urlsafe())
